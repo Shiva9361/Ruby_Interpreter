b7f6bdfb4010c2c5fac05aeb02b08b198a10601c
Shiva936 <shivadharshansankar936@gmail.com>
2023-10-26 21:56:50 +0530

    Created scanner class

---
+++ ruby/Scanner.java
@@ -0,0 +1,267 @@
+package ruby;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static ruby.TokenType.*;
+public class Scanner {
+
+    private static final Map<String,TokenType> keywords; 
+    
+    static{
+        keywords = new HashMap<>();
+        keywords.put("BEGIN",BEGIN_C);
+        keywords.put("END",END_C);
+        //keywords.put("alias",);
+        keywords.put("and",AND);
+        keywords.put("begin",BEGIN);
+        keywords.put("break",BREAK);
+        keywords.put("case",CASE);
+        keywords.put("class",CLASS);
+        keywords.put("def",DEF);
+        //keywords.put("defined?");
+        keywords.put("do",DO);
+        keywords.put("else",ELSE);
+        keywords.put("elsif",ELSIF);
+        keywords.put("end",END);
+        keywords.put("ensure",ENSURE);
+        keywords.put("false",FALSE);
+        keywords.put("for",FOR);
+        keywords.put("if",IF);
+        keywords.put("in",IN);
+        keywords.put("module",MODULE);
+        keywords.put("next",NEXT);
+        keywords.put("nil",NIL);
+        keywords.put("not",NOT);
+        keywords.put("or",OR);
+        //keywords.put("redo",);
+        keywords.put("rescue",RESCUE);
+        keywords.put("retry",RETRY);
+        keywords.put("return",RETURN);
+        keywords.put("self",SELF);
+        keywords.put("super",SUPER);
+        keywords.put("then",THEN);
+        keywords.put("true",TRUE);
+        //keywords.put("undef",);
+        keywords.put("unless",UNLESS);
+        //keywords.put("until",);
+        keywords.put("when",WHEN);
+        keywords.put("while",WHILE);
+        //keywords.put("yield");
+
+    }
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start =0;
+    private int current =0;
+    private int line = 1;
+    Scanner(String source){
+        this.source=source;
+    }
+
+    List<Token> scanTokens(){
+        while(!isAtEnd()){
+            // beg of next lex
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF,"",null,line));
+        return tokens;
+    }
+    
+
+
+    private void scanToken(){
+        char c = advance();
+        
+        switch (c){
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case '[': addToken(LEFT_SQUARE); break;
+            case ']': addToken(RIGHT_SQUARE); break;
+            
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(match('=') ? MINUS_EQUAL : MINUS); break;
+            case '+': addToken(match('=') ? PLUS_EQUAL : PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '?': addToken(QUESTION_MARK); break;
+            case '%': addToken(MOD); break;
+            case '*': addToken(match('=') ? STAR_EQUAL : STAR); break;
+            
+            case '!':addToken(match('=') ? BANG_EQUAL : BANG);break;
+            case '=':addToken(match('=') ? EQUAL_EQUAL : EQUAL);break;
+            
+            case '<':
+                if (match('=')){
+                    addToken(LESS_EQUAL);
+                }
+                else if (match('<')){
+                    addToken(LEFT_SHIFT);
+                }
+                else{
+                    addToken(LESS);
+                }
+                break;
+            case '>':
+                if (match('=')){
+                    addToken(GREATER_EQUAL);
+                }
+                else if (match('>')){
+                    addToken(RIGHT_SHIFT);
+                }
+                else{
+                    addToken(GREATER);
+                }
+                break;
+            
+            case ':':addToken(match(':') ? COLON_COLON: COLON); break;
+            case '&':addToken(match('&') ? AMPERSAND_AMPERSAND: AMPERSAND); break;
+            case '|':addToken(match('|') ? PIPE_PIPE : PIPE);
+            case '^':addToken(KARROT);
+            case '~':addToken(TILDA);
+
+            case '/':
+                if (match('/')){
+                    while(peek()!='\n' && !isAtEnd()) advance();
+                }
+                else if (match('=')){
+                    addToken(SLASH_EQUAL);
+                }
+                else{
+                    addToken(SLASH);
+                }
+                break;
+            
+            // Ignoring all kinds of white spaces
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            
+            // int values
+            case '1':case '2':case '3':case '4':case '5':case '6':
+            case '7':case '8':case '9':case '0':
+                number();
+                break;
+            //String 
+            case '"': string(); break;
+            
+            default:
+                if (isAlpha(c)){
+                    identifier();
+                }
+                Ruby.error(line, "Unexpected Character");
+                break;
+            }
+        }
+    
+
+    /*
+     * Helper Functions
+     */
+
+    private boolean isAtEnd(){
+         return current >=source.length();
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+    
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+    
+    private boolean match(char expected){
+
+        // Current is already increamented when advance is called 
+        // That's why we need to check "CURRENT" now
+        if (isAtEnd()) return false;
+        if (source.charAt(current) !=expected) return false;
+
+        current++;
+        return true;
+    }
+    // Dosen't increment current 
+    private char peek(){
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private void string(){
+        /*
+         * Ruby supports multiline strings...
+         */
+        while (peek()!='"' && !isAtEnd()){
+            if (peek() == '\n') line++;
+            advance();
+        }
+        if (isAtEnd()){
+            Ruby.error(line,"Unterminated String");
+            return;
+        }
+        // This is for the closing "
+        advance();
+
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
+    }
+
+    private boolean isDigit(char c){
+        return c>='0' && c<='9';
+    }
+
+    private char peekNext(){
+        if (current+1>=source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+    private void number(){
+        boolean isFloat = false;
+        while(isDigit(peek()) || peek()=='.') advance();
+        // Not considering method, have to change
+        if (peek()=='.' && isDigit(peekNext())){
+            isFloat = true;
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        if (!isFloat){
+            addToken(INTEGER, Integer.parseInt((source.substring(start, current))));
+        }
+        else{
+            addToken(FLOAT, Float.parseFloat((source.substring(start, current))));
+        }
+        
+    }
+    private void identifier(){
+        while(isAlphanumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type==null) type = IDENTIFIER;
+        addToken(type);
+    } 
+    
+    private boolean isAlpha(char c){
+        return  (c>= 'a'&& c<='z') || 
+                (c>='A' && c<='Z') || 
+                c == '_';
+    }
+
+    private boolean isAlphanumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+
+}
